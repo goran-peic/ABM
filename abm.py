107f4dea17170baf2af00b5562535e0a2d518560
Goran <goran.peic@gmail.com>
2023-12-18 00:48:40 -0500

    Update abm.py

--- abm.py
+++ abm.py
@@ -84,8 +84,13 @@ def index():
     # creature_plot.legend.label_text_font_style = "bold"
 
     html_text = file_html(creature_plot, CDN, "Population Evolution")
-    element_id = html_text[
-                 re.search("elementid", html_text).start() + 12: re.search("elementid", html_text) + 48]
+    try:
+      element_id = html_text[
+                 re.search("elementid", html_text).start() + 12: re.search("elementid", html_text).start() + 48]
+      except AttributeError:
+                element_id = html_text[
+                re.search("elementid", html_text) + 12: re.search("elementid", html_text) + 48]
+        
     js_script = html_text[re.search(r"function()", html_text).start() - 8 : re.search("Bokeh.embed.embed_items",
                                                                                       html_text).start() + 61]
 
